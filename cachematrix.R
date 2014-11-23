895fad5ad1877cb84d94917164424aebd85c61ae
Junior-Rios <jcd_rios@hotmail.com>
2014-11-23 16:02:18 -0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,15 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## The goal is to write two function to create , store and retrieve matrix and its inverse from its own cache.
+
 ## Write a short comment describing this function
+
+## MakeCacheMatrix Creates a type of custom matrix able to perform four functions :
+# 1 -Set the value of the matrix;
+# 2- Get the value of the matrix;
+# 3 -Set the value of the matrix;
+# 4 -Get the value of the matrix;
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -17,8 +25,12 @@ makeCacheMatrix <- function(x = matrix()) {
        getInverse = getInverse)
 }
 
-
 ## Write a short comment describing this function
+
+## This setença Calculates the inverse of the matrix If she has not already been calculated previously. 
+## If the result is in memory , it will print only the previous result .
+
+        ## Return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
   m <- x$getInverse()                 
@@ -29,5 +41,4 @@ cacheSolve <- function(x, ...) {
   data <- x$get()                      
   m <- solve(data, ...)               
   x$setInverse(m)
-        ## Return a matrix that is the inverse of 'x'
 }
